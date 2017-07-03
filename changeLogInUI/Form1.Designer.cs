141f2f9d3656d3a4e3ff5a07a2defcfe1dbb93f0
Mohammad Zaif Quraishi <mzaifquraishi@users.noreply.github.com>
2017-07-03 12:03:13 +0530

    Add files via upload

---
+++ changeLogInUI/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace changeUi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.usrn = new System.Windows.Forms.Label();
+            this.setreg = new System.Windows.Forms.Button();
+            this.brws = new System.Windows.Forms.Button();
+            this.resetreg = new System.Windows.Forms.Button();
+            this.setImg = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(428, 366);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(158, 97);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(84, 76);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 0;
+            this.pictureBox2.TabStop = false;
+            // 
+            // usrn
+            // 
+            this.usrn.AutoSize = true;
+            this.usrn.Location = new System.Drawing.Point(182, 189);
+            this.usrn.Name = "usrn";
+            this.usrn.Size = new System.Drawing.Size(35, 13);
+            this.usrn.TabIndex = 1;
+            this.usrn.Text = "label1";
+            // 
+            // setreg
+            // 
+            this.setreg.Location = new System.Drawing.Point(119, 259);
+            this.setreg.Name = "setreg";
+            this.setreg.Size = new System.Drawing.Size(75, 23);
+            this.setreg.TabIndex = 2;
+            this.setreg.Text = "create reg.";
+            this.setreg.UseVisualStyleBackColor = true;
+            this.setreg.Click += new System.EventHandler(this.setreg_Click);
+            // 
+            // brws
+            // 
+            this.brws.Location = new System.Drawing.Point(212, 223);
+            this.brws.Name = "brws";
+            this.brws.Size = new System.Drawing.Size(75, 23);
+            this.brws.TabIndex = 2;
+            this.brws.Text = "browse Image";
+            this.brws.UseVisualStyleBackColor = true;
+            this.brws.Click += new System.EventHandler(this.brws_Click_1);
+            // 
+            // resetreg
+            // 
+            this.resetreg.Location = new System.Drawing.Point(212, 259);
+            this.resetreg.Name = "resetreg";
+            this.resetreg.Size = new System.Drawing.Size(75, 23);
+            this.resetreg.TabIndex = 2;
+            this.resetreg.Text = "reset";
+            this.resetreg.UseVisualStyleBackColor = true;
+            this.resetreg.Click += new System.EventHandler(this.resetreg_Click);
+            // 
+            // setImg
+            // 
+            this.setImg.Location = new System.Drawing.Point(119, 223);
+            this.setImg.Name = "setImg";
+            this.setImg.Size = new System.Drawing.Size(75, 23);
+            this.setImg.TabIndex = 2;
+            this.setImg.Text = "setImg";
+            this.setImg.UseVisualStyleBackColor = true;
+            this.setImg.Click += new System.EventHandler(this.setImg_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(428, 366);
+            this.Controls.Add(this.setImg);
+            this.Controls.Add(this.resetreg);
+            this.Controls.Add(this.brws);
+            this.Controls.Add(this.setreg);
+            this.Controls.Add(this.usrn);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label usrn;
+        private System.Windows.Forms.Button setreg;
+        private System.Windows.Forms.Button brws;
+        private System.Windows.Forms.Button resetreg;
+        private System.Windows.Forms.Button setImg;
+    }
+}
+
---
+++ changeLogInUI/Form1.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using Microsoft.Win32;
+
+namespace changeUi
+{
+    public partial class Form1 : Form
+    {
+        string p = @"C:\\Windows\\System32\\oobe\\info\\backgrounds\\";
+
+        public Form1()
+        {
+            InitializeComponent();
+            string user2 = Environment.UserName; //extract username
+            usrn.Text = user2;          //pass username to label usrn
+            pictureBox2.Image = new Bitmap(GetUserimage()); // pass profile pic to picturebox
+
+
+        }
+
+
+
+        public static Image GetUserimage()
+        {
+            if (File.Exists(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + @"\Temp\" + Environment.UserName + ".bmp"))
+            {
+                return Image.FromFile(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + @"\Temp\" + Environment.UserName + ".bmp");
+            }
+            else
+            {
+                string p = @"C:\Windows\Web\Wallpaper\Windows\img0.jpg";
+                return Image.FromFile(p);
+            }
+        }
+
+        private void brws_Click_1(object sender, EventArgs e)
+        {
+            OpenFileDialog open = new OpenFileDialog();
+            open.Filter = "Image File|*.bmp|*.gif|*.jpe|*.jpeg|*.jpg|*.png|*.srf|*.tga|*.tif|*.tiff|*.webp|All Files|*.*";
+            if (open.ShowDialog() == DialogResult.OK)
+            {
+                File.Copy(open.FileName, p + "temp.jpg", true);
+                pictureBox1.Image = new Bitmap(open.FileName);
+            }
+
+            else
+            {
+                MessageBox.Show("File couldn't be open");
+            }
+
+        }
+
+        private void setreg_Click(object sender, EventArgs e)
+        {
+         try
+            {
+
+                RegistryKey rkey;
+                rkey = Registry.LocalMachine.CreateSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Authentication\\LogonUI\\Background");
+                rkey.SetValue("OEMBackground", "1", RegistryValueKind.DWord);
+                rkey.Close();
+                MessageBox.Show("done");
+            }
+            catch (Exception er)
+            {
+                MessageBox.Show("UnKnown Error == " + er);
+            }
+        }
+
+        private void setImg_Click(object sender, EventArgs e)
+        {
+        if (File.Exists(p + "backgroundDefault.jpg"))
+            {
+                File.Delete(p + "backgroundDefault.jpg");
+            }
+            File.Copy(p + "temp.jpg", p + "backgroundDefault.jpg", true);
+        }
+
+        private void resetreg_Click(object sender, EventArgs e)
+        {
+             try
+            {
+                string keyName = @"SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Authentication\\LogonUI\\Background";
+                using (RegistryKey key = Registry.LocalMachine.OpenSubKey(keyName, true))
+                {
+                    if (key == null)
+                    {
+                        MessageBox.Show("not set");
+                    }
+                    else
+                    {
+                        File.Delete(p + "backgroundDefault.jpg");
+                        key.DeleteValue("OEMBackground");
+                        MessageBox.Show("Deleted");
+                    }
+                }
+            }
+            catch (Exception er)
+            {
+                MessageBox.Show("UnKnown Error == " + er);
+            }
+        
+        
+        }
+
+        
+
+
+
+
+    }
+}
